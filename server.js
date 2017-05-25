60ab337ecf7a49a6eb2606ac38b4d146e0ad682f
HienNguyen <nguyenconghien0102@gmail.com>
2017-05-25 09:53:58 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var server = require("http").createServer(app);
 //var io = require("socket.io").listen(server);
 //var fs = require("fs");
-server.listen(process.env.PORT || 80);
+server.listen(process.env.PORT || 3000);
 
 app.get("/", function(req, res){
 	res.sendFile(__dirname + "/index.html");	
